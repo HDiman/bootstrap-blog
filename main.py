31b507a1627953363eb9a4cca5b86cd10c6f1baf
Dmitry Sannikov <HDiman@users.noreply.github.com>
2022-05-19 16:38:16 +0300

    Adding posts through API

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
+def get_all_posts():
+    blog_url = "https://api.npoint.io/8169b5687e2da25a607c"
+    response = requests.get(blog_url)
+    return response.json()
+
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", all_posts=get_all_posts(), posts_num=len(get_all_posts()))
 
 @app.route("/about")
 def get_about():
@@ -14,9 +21,10 @@ def get_about():
 def get_contact():
     return render_template("contact.html")
 
-@app.route("/post")
-def get_post():
-    return render_template("post.html")
+@app.route("/post/<num>")
+def get_blog(num):
+    return render_template("post.html", all_posts=get_all_posts(), num=int(num))
+
 
 
 if __name__ == '__main__':
